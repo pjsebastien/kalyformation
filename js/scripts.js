c075eba156d810fe5f2dda83a92857828fc52386
pierrejean <pj.sebastien@gmail.com>
2021-12-02 12:22:21 +0400

    insert docs link and pdf

--- js/scripts.js
+++ js/scripts.js
@@ -166,17 +166,17 @@
     // });
 
     /* Details Lightbox - Magnific Popup */
-    $('.popup-with-move-anim').magnificPopup({
-        type: 'inline',
-        fixedContentPos: false /* keep it false to avoid html tag shift with margin-right: 17px */,
-        fixedBgPos: true,
-        overflowY: 'auto',
-        closeBtnInside: true,
-        preloader: false,
-        midClick: true,
-        removalDelay: 300,
-        mainClass: 'my-mfp-slide-bottom',
-    });
+    // $('.popup-with-move-anim').magnificPopup({
+    //     type: 'inline',
+    //     fixedContentPos: false /* keep it false to avoid html tag shift with margin-right: 17px */,
+    //     fixedBgPos: true,
+    //     overflowY: 'auto',
+    //     closeBtnInside: true,
+    //     preloader: false,
+    //     midClick: true,
+    //     removalDelay: 300,
+    //     mainClass: 'my-mfp-slide-bottom',
+    // });
 
     // /* Move Form Fields Label When User Types */
     // // for input and textarea fields
